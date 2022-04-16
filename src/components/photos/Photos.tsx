067359dff120e89507f81b4b841d382cea654acf
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-04-16 16:54:04 +0300

    added fetching of photos

--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -5,49 +5,23 @@ import { SmallPhotos } from "../../photoInterface/PhotoTypes";
 
 import classes from "./Photos.module.css";
 
+const API_LINK = "https://boiling-refuge-66454.herokuapp.com/images";
+
 export default function Photos() {
-  const dummyPhoto: SmallPhotos = [
-    {
-      id: 0,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 1,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 2,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 3,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 4,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 5,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 6,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 7,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-    {
-      id: 8,
-      url: "https://i.picsum.photos/id/237/300/200.jpg?hmac=WSdbBEXvCVSqNN1HnCzm7ohp6DhAJfl9t3TcqBNDn_Q",
-    },
-  ];
+  const [posts, setPosts] = useState<SmallPhotos>([]);
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      const responce = await fetch(API_LINK);
+      const responceData: SmallPhotos = await responce.json();
+      setPosts(responceData);
+    }
+    fetchPhotos();
+  }, []);
 
   return (
     <div className={classes.photosContainer}>
-      {dummyPhoto.map((item) => {
+      {posts.map((item) => {
         return <Photo id={item.id} url={item.url} key={item.id} />;
       })}
     </div>
